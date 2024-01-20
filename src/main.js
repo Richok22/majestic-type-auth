6f32182699e069d11ea058f96c3b8765678be3dd
Richok22 <75800944+Richok22@users.noreply.github.com>
2024-01-20 15:11:42 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,22 +3,12 @@ import "/src/assets/main.css"
 import { createApp } from 'vue';
 import { createPinia } from 'pinia'
 import App from './App.vue';
-<<<<<<< HEAD
 import router from '/src/routes/routes.js'
 
 const pinia = createPinia();
 
-=======
 import { createRouter, createWebHashHistory } from 'vue-router';
 import { routes } from './routes/routes.js';
-
-const pinia = createPinia();
-
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-});
->>>>>>> 8fcb3449419c3401b43597f6b57b331bd8fc567e
 
 const app = createApp(App);
 app.use(pinia);
