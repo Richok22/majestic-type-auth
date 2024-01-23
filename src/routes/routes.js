c4fbca582827dd0bca328b93697f4ffa4524a6cd
Richok22 <75800944+Richok22@users.noreply.github.com>
2024-01-23 19:59:45 +0200

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 import { createRouter, createWebHistory } from 'vue-router'
 import auth from "@/views/auth.vue";
 import chooseMenu from "@/views/chooseMenu.vue";
 
-let routes = [
+export let routes = [
     {
         path: '/',
         name: 'auth',
@@ -22,24 +21,3 @@ const router = createRouter({
 })
 
 export default router
-=======
-// Assuming this is your routes.js file
-import { createRouter, createWebHistory } from 'vue-router';
-import Auth from '/src/views/auth.vue';
-import App from '/src/App.vue';
-
-export const routes = [
-    {
-        name: 'auth',
-        path: '/',
-        component: Auth
-    },
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
-
-export default router;
->>>>>>> 8fcb3449419c3401b43597f6b57b331bd8fc567e
